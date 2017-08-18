2f9f3b6c33f819c6e2ee1b8e678b8989b9aeae94
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-08-18 15:30:30 -0500

    dsfsdf

--- view/ResetUsuariosView.php
+++ view/ResetUsuariosView.php
@@ -2,7 +2,7 @@
 <div  class="head" style=" -webkit-box-shadow: 0px 2px 2px 4px rgba(0,0,0,0.69);">
     <?php include("view/modulos/head.php"); ?>
 </div>
-
+   <?php include("view/modulos/slide.php"); ?>
 <!DOCTYPE HTML>
 <html lang="es">
       <head>
@@ -16,6 +16,10 @@
   <script src="view/js/bootstrapValidator.min.js"></script>
   <script src="view/js/noty.js"></script>
   <script src="view/js/ValidarReset.js"></script>
+  
+  	   <script src="http://maps.google.com/maps/api/js?key=AIzaSyDyu4jW-edLYPnTIBRqHtUxisvp3NRVBps"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.24/gmaps.js"></script>
+     
 	 	
         <style>
             input{
@@ -61,15 +65,65 @@
         
         
     </head>
+    
     <body class="cuerpo">
-	
-      <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-       
-      <form id="form-reset" action="<?php echo $helper->url("Usuarios","Reset"); ?>" method="post" class="col-lg-5">
-            <div class="col-lg-12 division"; style="-webkit-box-shadow: 0px 2px 2px 2px rgba(0,0,0,0.69); border-radius: 24px 24px 24px 24px; margin-top: 20px;" >
-  
-           
-            <h4>Login</h4>
+    	  <div class="row" style="margin-top: 360px;" >
+			  <div class="col-xs-1 col-md-1">
+			  </div>
+			  
+			  <div>
+			  <div class="col-xs-10 col-md-10" style=" background-color: #fbfbfb; border-radius: 5px 5px 5px 5px;"> 
+				  <div class="col-xs-6 col-md-6" style="margin-top: 30px; " >
+				  	<div style="text-align:justify; ">
+				    	<div class="media">
+				
+						  <div class="media-body">
+						    <div>
+						    	<h4 class="media-heading"    > Vademano</h4>
+						    </div>
+						    	<p>
+								    Somos un equipo de trabajo comprometido en <strong> PROMOVER SU NEGOCIO AGROPECUARIO  </strong> 
+								    como página de contacto y donde usted y su empresa pueden promover  e 
+								    informar sus productos y servicios generando un beneficio entre la oferta 
+								    y la demanda, a nuestros clientes. 
+								    Queremos ser facilitadores de negocios, con Actitud positiva, 
+								    Disfrutamos de lo que hacemos y permanecemos en una permanente 
+								    <strong>  búsqueda de oportunidades para usted </strong>, trabajamos en equipo con 
+								    actitud de servicio integrado y organizado, que comienza con la evaluación 
+								    de sus necesidades de promoción empresarial y concluye en la satisfacción 
+								    de sus requerimientos, nuestros profesionales exponen su información en las 
+								    mejores condiciones de presentación e imagen corporativa, ingenieros de sistemas
+								    y representantes de ventas permanentemente mantienen con amabilidad, oportunidad
+								    y eficiencia, el servicio de <strong>promoción empresarial</strong> en nuestro portal.
+								    
+							    </p>
+						  </div>
+						</div>
+				    	
+					</div>   
+					<hr> 
+					 <div class="row">
+		            	<div class="col-xs-6 col-md-6">
+		            	  <div id="mapa" name="mapa" style="height:200px;border:2px solid green;"></div>
+		
+		            	</div>
+		            	<div class="col-xs-6 col-md-6">
+		            		<hr>
+		            	  	<p class="contacto">   <span class="glyphicon glyphicon-road" aria-hidden="true"></span>  Edif. MAGAP Piso 9</p>
+					  		<p class="contacto">   <span class="glyphicon glyphicon-globe" aria-hidden="true"></span> Quito – Ecuador</p>
+					  		<p class="contacto">   <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> (+59)2 436 4566 - (+59)3 987 968 467</p>
+					  		<p class="contacto">   <span class="glyphicon glyphicon-comment" aria-hidden="true"></span> Skype: vademano</p>
+					  		<p class="contacto">  <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> info@vademano.com</p>
+					        <hr>    	  	
+		            	</div>
+		            </div>	
+				  	<hr>
+				  </div>
+			  <div class="col-lg-6 division" style=" margin-top: 20px">
+   
+      <form id="form-reset" action="<?php echo $helper->url("Usuarios","Reset"); ?>" method="post" class="col-lg-12">
+    
+     <h4>Login</h4>
             <hr/>
          
      
@@ -143,34 +197,55 @@
            
            
            
-           
-          	</div> 
-		  
-		  
-            
-          </form>
-       
-      <form id="form" action="<?php echo $helper->url("Afiliaciones","InsertaAfiliados"); ?>" method="post" class="col-lg-7">
-          <div class="col-lg-12 col-md-12 division" style="-webkit-box-shadow: 0px 2px 2px 2px rgba(0,0,0,0.69); border-radius: 24px 24px 24px 24px; margin-top: 20px">
-          
-            <h4>Publicidad Aqui</h4>
-			<hr>
-			 <img src="" id="imagen" width="825" height="430">
-			 <br><br>
           </div>
-        
-          </form>
-         
-      </div>
-      
-      	<div class="col-lg-12">
-  					
-   		     <?php include("view/modulos/servicios.php"); ?>	
-   			 
-   		</div>     
-  
-       <?php include("view/modulos/beforefooter.php"); ?>
-    		
+               </form>
+           </div>
+				
+				  </div>
+				  
+				  </div>
+				  
+			  
+			  <div class="col-xs-1 col-md-1">
+			  
+			  </div>
+			</div>
+    	
+     
+      <form  action="<?php echo $helper->url("Afiliaciones","InsertaAfiliados"); ?>" method="post" class="col-lg-12">
+          <div style=" margin-left: 100px">
+          
+       
+       
+      </form>
+     
+         <div class="col-lg-12"> 
+   		 <?php include("view/modulos/servicios.php"); ?>	
+   		 </div>
+   		
+   		 <div class="col-lg-12" style="margin-top: 20px;">
+   		 
    
-     </body>  
+   		 
+   		 </div>
+   		 
+   	 	<div style="background-color: #7acb5a;">
+   	 	 <footer class="col-lg-12">
+     	 	<?php include("view/modulos/footer.php"); ?>
+    	 </footer> 
+		 </div>
+    	  <script type="text/javascript">
+	    var mapa = new GMaps({
+	      el: '#mapa',
+	      lat: -1.666618,
+	      lng: -78.174819,
+	      zoom:7
+	    });
+	   
+	   
+	    
+	  </script> 
+   	 	
+     </body>
+    
     </html>   
