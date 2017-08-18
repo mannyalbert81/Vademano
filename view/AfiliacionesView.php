a44fddadef3ed181cb256c211e0a6a55184ccd12
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-08-18 13:49:33 -0500

    addas

--- view/AfiliacionesView.php
+++ view/AfiliacionesView.php
@@ -15,6 +15,10 @@
   
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 	
+	
+   <script src="http://maps.google.com/maps/api/js?key=AIzaSyDyu4jW-edLYPnTIBRqHtUxisvp3NRVBps"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.24/gmaps.js"></script>
+     
 		
  		<script>
 		$(document).ready(function(){
@@ -375,15 +379,63 @@
  		
  		
     </head>
-    <center>
+    
     <body class="cuerpo">
-
-     
-      <form  action="<?php echo $helper->url("Afiliaciones","InsertaAfiliados"); ?>" method="post" class="col-lg-12">
-          <div style=" margin-left: 100px">
-            <div class="col-lg-11 division" style=" margin-top: 20px">
+    	  <div class="row" style="margin-top: 370px;" >
+			  <div class="col-xs-1 col-md-1">
+			  </div>
+			  
+			  <div>
+			  <div class="col-xs-10 col-md-10" style=" background-color: #fbfbfb; border-radius: 5px 5px 5px 5px;"> 
+				  <div class="col-xs-6 col-md-6" style="margin-top: 30px; " >
+				  	<div style="text-align:justify; ">
+				    	<div class="media">
+				
+						  <div class="media-body">
+						    <div>
+						    	<h4 class="media-heading"    > Vademano</h4>
+						    </div>
+						    	<p>
+								    Somos un equipo de trabajo comprometido en <strong> PROMOVER SU NEGOCIO AGROPECUARIO  </strong> 
+								    como página de contacto y donde usted y su empresa pueden promover  e 
+								    informar sus productos y servicios generando un beneficio entre la oferta 
+								    y la demanda, a nuestros clientes. 
+								    Queremos ser facilitadores de negocios, con Actitud positiva, 
+								    Disfrutamos de lo que hacemos y permanecemos en una permanente 
+								    <strong>  búsqueda de oportunidades para usted </strong>, trabajamos en equipo con 
+								    actitud de servicio integrado y organizado, que comienza con la evaluación 
+								    de sus necesidades de promoción empresarial y concluye en la satisfacción 
+								    de sus requerimientos, nuestros profesionales exponen su información en las 
+								    mejores condiciones de presentación e imagen corporativa, ingenieros de sistemas
+								    y representantes de ventas permanentemente mantienen con amabilidad, oportunidad
+								    y eficiencia, el servicio de <strong>promoción empresarial</strong> en nuestro portal.
+								    
+							    </p>
+						  </div>
+						</div>
+				    	
+					</div>   
+					<hr> 
+					 <div class="row">
+		            	<div class="col-xs-6 col-md-6">
+		            	  <div id="mapa" name="mapa" style="height:200px;border:2px solid green;"></div>
+		
+		            	</div>
+		            	<div class="col-xs-6 col-md-6">
+		            		<hr>
+		            	  	<p class="contacto">   <span class="glyphicon glyphicon-road" aria-hidden="true"></span>  Edif. MAGAP Piso 9</p>
+					  		<p class="contacto">   <span class="glyphicon glyphicon-globe" aria-hidden="true"></span> Quito – Ecuador</p>
+					  		<p class="contacto">   <span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> (+59)2 436 4566 - (+59)3 987 968 467</p>
+					  		<p class="contacto">   <span class="glyphicon glyphicon-comment" aria-hidden="true"></span> Skype: vademano</p>
+					  		<p class="contacto">  <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> info@vademano.com</p>
+					        <hr>    	  	
+		            	</div>
+		            </div>	
+				  	<hr>
+				  </div>
+			  <div class="col-lg-6 division" style=" margin-top: 20px">
             <h4>Afiliación</h4>
-            <hr/>
+         
            <?php if (isset($resultado)) {?>
         	<?php if ($resultado == "true") {?>
         	
@@ -723,6 +775,21 @@
 		     <?php }?>
 			  <br>
            </div>
+				
+				  </div>
+				  
+				  </div>
+				  
+			  
+			  <div class="col-xs-1 col-md-1">
+			  
+			  </div>
+			</div>
+    	
+     
+      <form  action="<?php echo $helper->url("Afiliaciones","InsertaAfiliados"); ?>" method="post" class="col-lg-12">
+          <div style=" margin-left: 100px">
+          
        
        
       </form>
@@ -743,6 +810,18 @@
     	 </footer> 
     	     
     	</div>
+    	  <script type="text/javascript">
+	    var mapa = new GMaps({
+	      el: '#mapa',
+	      lat: -1.666618,
+	      lng: -78.174819,
+	      zoom:7
+	    });
+	   
+	   
+	    
+	  </script> 
+   	 	
      </body>  
     	
     	
