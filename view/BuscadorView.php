fad9bcc59d3762cbbd768c10ea84a25887523f40
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-08-21 12:32:17 -0500

    fg

--- view/BuscadorView.php
+++ view/BuscadorView.php
@@ -25,7 +25,24 @@
 				});	
 			});
 	</script>
+<script >
+p {
+	  animation-duration: 3s;
+	  animation-name: slidein;
+	}
 
+	@keyframes slidein {
+	  from {
+	    margin-left: 100%;
+	    width: 300%
+	  }
+
+	  to {
+	    margin-left: 0%;
+	    width: 100%;
+	  }
+	}
+</script>
   	
   	<script >
 		$(document).ready(function(){
