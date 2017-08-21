8988f316d1fb088bcaf5d1686bdb8d359812ffe5
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-08-21 12:32:44 -0500

    cxv

--- view/BuscadorView.php
+++ view/BuscadorView.php
@@ -26,22 +26,6 @@
 			});
 	</script>
 <script >
-p {
-	  animation-duration: 3s;
-	  animation-name: slidein;
-	}
-
-	@keyframes slidein {
-	  from {
-	    margin-left: 100%;
-	    width: 300%
-	  }
-
-	  to {
-	    margin-left: 0%;
-	    width: 100%;
-	  }
-	}
 </script>
   	
   	<script >
