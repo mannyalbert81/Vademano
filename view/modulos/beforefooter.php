7f5f019af001b63b5ebfd3a7ad3c4a5195f2a8fc
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-08-31 15:51:39 -0500

    sdf

--- view/modulos/beforefooter.php
+++ view/modulos/beforefooter.php
@@ -1,5 +1,4 @@
- <script src="http://maps.google.com/maps/api/js?key=AIzaSyDyu4jW-edLYPnTIBRqHtUxisvp3NRVBps"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.24/gmaps.js"></script>
+
 
 <div class="navbar navbar-bottom"  >
 <footer>
