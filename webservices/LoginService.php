811c43af60e83bfc1a7f8a00cb6d8dbe7e596569
desarrollo <desarrollo@masoft.net>
2017-09-25 17:59:23 -0500

    ff

--- webservices/LoginService.php
+++ webservices/LoginService.php
@@ -4,44 +4,23 @@ require_once '../core/DB_Functions.php';
 $db = new DB_Functions();
 
 $accion=(isset($_POST['action']))?$_POST['action']:'';
-	
+$_id_usuario  = (isset($_POST['id_usuario']))?$_POST['id_usuario']:'';	
 
 if($accion=="consulta"){
 
 	
-$columnas = "fichas_service.id_fichas, 
-  fichas_service.nombre_fichas, 
-  fichas_service.encabezado_tabla_fichas, 
-  fichas_service.farmacocinetica_fichas, 
-  fichas_service.accion_terapeutica_fichas, 
-  fichas_service.clasificacion_farmacologica_fichas, 
-  fichas_service.forma_terapeutica_fichas, 
-  fichas_service.indicaciones_uso_fichas, 
-  fichas_service.interacciones_fichas, 
-  fichas_service.contraindicaciones_fichas, 
-  fichas_service.periodo_retiro_fichas, 
-  fichas_service.advertencias_fichas, 
-  fichas_service.presentacion_fichas, 
-  fichas_service.registro_sanitario_fichas, 
-  fichas_service.id_fichas_fotos, 
-  fichas_service.consultas_fichas, 
-  fichas_service.buscador, 
-  fichas_service.mecanismo_accion_fichas, 
-  fichas_service.conservacion_fichas, 
-  fichas_service.efectos_colaterales_fichas, 
-  fichas_service.ingredientes_fichas, 
-  fichas_service.tipo_alimento_fichas, 
-  fichas_service.encabezado_dosificacion_fichas, 
-  fichas_service.tipo_ficha, 
-  fichas_service.tabla_formas_administracion, 
-  fichas_service.tabla_laboratorios, 
-  fichas_service.tabla_distribuidores, 
-  fichas_service.tabla_composicion, 
-  fichas_service.tabla_dosificacion";
+$columnas = "estado.nombre_estado, 
+  			usuarios.id_usuario,
+			usuarios.nombres_usuario, 
+ 			usuarios.apellidos_usuario, 
+ 			 usuarios.usuario_usuario, 
+  			usuarios.celular_usuario, 
+  			usuarios.telefono_usuario";
 
 
-$talba = "public.fichas_service";
-$where = "1=1";
+$talba = "public.usuarios, 
+  		  public.estado";
+$where = " id_usuario = '$_id_usuario'  ";
 $resultado = $db->getCondiciones($columnas, $talba, $where);
 
 
