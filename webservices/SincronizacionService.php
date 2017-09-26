a5d3ccc85fd39a4cb150c591f6572681654cb47d
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-09-26 12:38:34 -0500

    fref

--- webservices/SincronizacionService.php
+++ webservices/SincronizacionService.php
@@ -3,47 +3,29 @@
 require_once '../core/DB_Functions.php';
 $db = new DB_Functions();
 
-$accion=(isset($_POST['action']))?$_POST['action']:'';
-$_id_usuario  = (isset($_POST['id_usuario']))?$_POST['id_usuario']:'';	
+function Insertar(){
 
-if(isset($_GET['idusuario']))
+if(isset($_GET['id_usuario']))
 {
-	Consulta
+	$columnas = "estado.nombre_estado,
+  				usuarios.id_usuario,
+				usuarios.nombres_usuario,
+ 				usuarios.apellidos_usuario,
+ 			 	usuarios.usuario_usuario,
+  				usuarios.celular_usuario,
+  				usuarios.telefono_usuario";
 	
-
-	$resultadosJson = json_encode($listfotos);
+	
+	$tabla = "public.usuarios,
+  		  public.estado";
+	$where = " id_usuario = '$_id_usuario'  ";
+	$resultado = $db->getCondiciones($columnas, $tabla, $where);
+	
+} 
+			$resultadosJson = json_encode($resultado);
+	}
 
 echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
-	
-}
 
-if($accion=="consulta"){
-
-	
-$columnas = "estado.nombre_estado, 
-  			usuarios.id_usuario,
-			usuarios.nombres_usuario, 
- 			usuarios.apellidos_usuario, 
- 			usuarios.usuario_usuario, 
-  			usuarios.celular_usuario, 
-  			usuarios.telefono_usuario";
-
-
-$tabla = "public.usuarios, 
-  		  public.estado";
-$where = " id_usuario = '$_id_usuario'  ";
-$resultado = $db->getCondiciones($columnas, $tabla, $where);
-
-
-echo json_encode($resultado);
-}
-else{
-	
-	
-	echo "no entro";
-}
-
-
-
-
+?>
 	
