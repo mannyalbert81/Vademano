cb009052b16c1e0b5727998017933d440eb4d2ea
dannymasoft <danny@masoft.net>
2017-09-28 13:06:52 -0500

    yu

--- webservices/SincronizacionService.php
+++ webservices/SincronizacionService.php
@@ -3,24 +3,42 @@
 require_once '../core/DB_Functions.php';
 $db = new DB_Functions();
 
-
-
 if(isset($_GET['id_usuario']))
 {
-	$columnas = "estado.nombre_estado,
-  				usuarios.id_usuario,
-				usuarios.nombres_usuario,
- 				usuarios.apellidos_usuario,
- 			 	usuarios.usuario_usuario,
-  				usuarios.celular_usuario,
-  				usuarios.telefono_usuario";
+	$_id_usuario=trim($_GET['id_usuario']);
+	$columnas = "es.nombre_estado,
+  				us.id_usuario,
+				us.nombres_usuario,
+ 				us.apellidos_usuario,
+ 			 	us.usuario_usuario,
+  				us.celular_usuario,
+  				us.telefono_usuario";
 	
 	
-	$tabla = "public.usuarios,
-  		  public.estado";
-	$where = " id_usuario = '$_id_usuario'  ";
+	$tabla = "usuarios us INNER JOIN  estado es
+				ON us.id_estado = es.id_estado";
+	$where = " es.nombre_estado='ACTIVO'  AND  us.id_usuario = '$_id_usuario'  ";
 	$resultado = $db->getCondiciones($columnas, $tabla, $where);
-	$resultadosJson = json_encode($resultado);
+	$resultadosJson="";
+	
+	if(!empty($resultado))
+	{
+		$listusuario = [];
+		foreach ($resultado as $res)
+		{
+			$rowlist = new stdClass();			
+			$rowlist->nombres = $res->nombres_usuario;
+			$rowlist->apellidos = $res->apellidos_usuario;
+			$rowlist->usuario = $res->usuario_usuario;
+			$rowlist->celular = $res->celular_usuario;
+			$rowlist->telefono = $res->telefono_usuario;
+			$listusuario[]=$rowlist;
+		}
+		$resultadosJson = json_encode($listusuario);
+	}else{ 
+		$resultadosJson=json_encode(array(array('nombres'=>1,'apellidos'=>1,'usuario'=>1,'celular'=>1,'telefono'=>1)));
+	}
+	
 	echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
 } 
 			
